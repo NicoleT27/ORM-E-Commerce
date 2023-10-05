e44c4ccd3d0a90b4eeef591a358d6910c5dc72c1
Nicole Torres <nicolejoan9@yahoo.com>
2023-10-05 10:07:59 -0400

    Updated models

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -7,7 +7,10 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  Category.findAll().then((categories) => res.json(categories));
+  Category.findAll({
+include:[Product],
+  })
+  .then((categories) => res.json(categories));
   // find all categories
   // be sure to include its associated Products
 });
@@ -17,22 +20,37 @@ router.get('/:id', (req, res) => {
      where: {
        id: req.params.id,
      },
-   }).then((categories) => {
-     res.json(categories);
+     include: [Product],
+   })
+   .then((category) => {res.json(category);
    });
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
+  Category.create(req.body)
+    .then((category) => res.json(category));
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
+  Category.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((category) => res.json(category));
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((category) => res.json(category));
   // delete a category by its `id` value
 });
 
